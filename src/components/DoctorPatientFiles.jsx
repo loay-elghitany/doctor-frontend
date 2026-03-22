87c8ebff154144dbefa6d1c7cc5f9ebbdc8ce464
loay-elghitany <loayelghitany10@gmail.com>
2026-03-22 13:54:15 +0200

    remove old file

--- src/components/DoctorPatientFiles.jsx
+++
@@ -1,6 +0,0 @@
-// This component has been removed as part of removing patient file management functionality
-export const DoctorPatientFiles = ({ patientId, appointmentId, onClose }) => {
-  return null;
-};
-
-export default DoctorPatientFiles;
--- src/components/PatientMedicalFiles.jsx
+++
@@ -1,6 +0,0 @@
-// This component has been removed as part of removing patient file management functionality
-export const PatientMedicalFiles = () => {
-  return null;
-};
-
-export default PatientMedicalFiles;
